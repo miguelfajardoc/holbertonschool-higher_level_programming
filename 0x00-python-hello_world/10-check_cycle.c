fde9800da1853e82924ff5c125c29f68be7f6655
miguelfajardoc <775@holbertonschool.com>
2019-04-30 04:00:19 +0000

    structure of 10 point

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * check_cycle - check if a list have a cycle
+ * @list: pointer to the list to check
+ * Return: 0 if no null
+ */
+int check_cycle(listint_t *list)
+{
+	if (list != NULL)
+		return (0);
+	return (1);
+}
