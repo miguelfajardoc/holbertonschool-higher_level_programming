0b01c8c425a65782493f027ce634c53556269053
miguelfajardoc <775@holbertonschool.com>
2019-05-02 00:37:57 +0000

    argv in pyton

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    n = len(argv) -1
+    if n == 1:
+        arg = "argument"
+    else:
+        arg = "arguments"
+    if n == 0:
+        print("{:d} {}.".format(n, arg))
+    else:
+        print("{:d} {}:".format(n, arg))
+    for i in range(1, n + 1):
+        print("{:d}: {}".format(i, argv[i]))
