26264ce0bd3f4dba840241e29d66d4a39a71b05b
miguelfajardoc <775@holbertonschool.com>
2019-05-10 01:51:54 +0000

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    maximo = -10000000
+    maximo = -10000000000
     if a_dictionary is None:
         return None
     for name, score in a_dictionary.items():
