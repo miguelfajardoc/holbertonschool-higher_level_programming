763325cdac075ac4bb26d73a0b37b22bd6c3fbad
miguelfajardoc <775@holbertonschool.com>
2019-05-20 23:38:29 +0000

    print in error

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -10,7 +10,7 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
     except (ValueError, TypeError) as error:
-        eprint(error)
+        eprint("Exception: {}".format(error))
         return (False)
     else:
         return (True)
