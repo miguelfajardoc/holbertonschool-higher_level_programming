698c7a9ecd3ba1ac71f3d6c169ac26f4e6aec8ce
miguelfajardoc <775@holbertonschool.com>
2019-05-20 23:32:31 +0000

    print in error

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except (ValueError, TypeError) as error:
+        eprint(error)
+        return (False)
+    else:
+        return (True)
