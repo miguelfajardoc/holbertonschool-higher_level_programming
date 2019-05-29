8ba1a70d237aa26197c3d289514a3292fb0ca692
miguelfajardoc <775@holbertonschool.com>
2019-05-29 01:41:26 +0000

    pep8 fixes

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(count =[0]):
+def magic_string(count=[0]):
     count[0] += 1
-    return ("Holberton, "* count[0])[:-2]
+    return ("Holberton, " * count[0])[:-2]
