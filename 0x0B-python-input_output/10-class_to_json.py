efec3e6457d271393c8bf13a3444950249c7c5b1
miguelfajardoc <775@holbertonschool.com>
2019-06-06 02:03:22 +0000

    class student json

---
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""JSON string file load and save"""
+
+
+def class_to_json(obj):
+    return obj.__dict__
---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""JSON Class"""
+
+
+class Student:
+    """Class student json"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs == None:
+            return self.__dict__
+        if type(attrs) == list:
+            for atribute in attrs:
+                if type(atribute) != str:
+                    return self.__dict__
+                dictionary = {}
+                for key in self.__dict__:
+                    for atribute in attrs:
+                        if key == atribute:
+                            dictionary[key] = self.__dict__[key]
+                return dictionary
