9adf697cc2f4317de0d6ec36ecb3c45a4eb045c6
miguelfajardoc <775@holbertonschool.com>
2019-08-09 20:35:10 +0000

    [add] filter states argv

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" select states with python MySQLdb where coincide with the arg
+"""
+
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    course = db.cursor()
+    course.execute("""SELECT * FROM states WHERE name=%s ORDER BY
+                   id ASC """, (argv[4], ))
+    rows = course.fetchall()
+    for i in rows:
+        print(i)
