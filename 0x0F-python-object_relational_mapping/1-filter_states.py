6b306e63dff9b30b0b6472fb8d7134a112af328a
miguelfajardoc <775@holbertonschool.com>
2019-08-09 20:20:11 +0000

    [add] search by N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" select states with python MySQLdb
+""" select states with python MySQLdb where star with N
 """
 
 if __name__ == "__main__":
@@ -9,7 +9,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
     course = db.cursor()
-    course.execute("SELECT * FROM states ORDER BY id ASC")
+    course.execute("""SELECT * FROM states WHERE name REGEXP '^[N]' ORDER BY
+                   id ASC """)
     rows = course.fetchall()
     for i in rows:
         print(i)
