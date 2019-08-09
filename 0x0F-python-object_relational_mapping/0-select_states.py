d89db562b4b6c19c59f1a2ebb3a002d7f4f86300
miguelfajardoc <775@holbertonschool.com>
2019-08-09 17:47:57 +0000

    [add] first query with python!

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" select states with python MySQLdb
+"""
+
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2],
+                         db=argv[3])
+    course = db.cursor()
+    course.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = course.fetchall()
+    for i in rows:
+        print(i)
