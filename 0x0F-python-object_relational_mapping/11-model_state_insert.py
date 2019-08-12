571a509dfa4cbd9b2c5e07871a2b2306cf005197
miguelfajardoc <775@holbertonschool.com>
2019-08-12 00:31:26 +0000

    insert louisiana

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
 
     session = Session()
 
-    new_state = State(name = "Louisiana")
+    new_state = State(name="Louisiana")
     session.add(new_state)
     session.commit()
     print(new_state.id)
