6c75147a69e0c7389926594763defbeabbd41e9e
miguelfajardoc <775@holbertonschool.com>
2019-08-21 02:56:53 +0000

    peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,31 +6,36 @@ def find_peak(list_of_integers):
     """ function that find a peak """
     if len(list_of_integers) is 0:
         return None
-    index = int(len(list_of_integers) / 2)
-    return (find_true_peak(list_of_integers, index))
+    index = 0
+    mitad = int(len(list_of_integers) / 2)
+    return (find_true_peak(list_of_integers, index, mitad))
 
 
-def find_true_peak(lista, index):
+def find_true_peak(lista, index, mitad):
     """ true function that find a peak """
     if index is not 0 and index is not len(lista) - 1:
         if lista[index] >= lista[index -
                                  1] and lista[index] >= lista[index + 1]:
             return (lista[index])
         elif lista[index - 1] >= lista[index + 1]:
-            index = index - int(min(len(lista) - index, index) / 2)
-            return (find_true_peak(lista, index))
+            index = index - mitad
+            mitad = int(mitad / 2)
+            return (find_true_peak(lista, index, mitad))
         else:
-            index = index + int(min(len(lista) - index, index) / 2)
-            return (find_true_peak(lista, index))
+            index = index + mitad
+            mitad = int(mitad / 2)
+            return (find_true_peak(lista, index, mitad))
     elif index is 0:
         if lista[index] >= lista[index + 1]:
             return (lista[index])
         else:
-            index = index + int(min(len(lista) - index, index) / 2)
-            return (find_true_peak(lista, index))
+            index = index + mitad
+            mitad = int(mitad / 2)
+            return (find_true_peak(lista, index, mitad))
     else:
         if lista[index] >= lista[index - 1]:
             return (lista[index])
         else:
-            index = index - int(min(len(lista) - index, index) / 2)
-            return (find_true_peak(lista, index))
+            index = index - mitad
+            mitad = int(mitad / 2)
+            return (find_true_peak(lista, index, mitad))
