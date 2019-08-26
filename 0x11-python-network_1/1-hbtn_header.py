3e99833841704c3e3e403d4d5bcfdbee6727e682
miguelfajardoc <775@holbertonschool.com>
2019-08-26 22:22:32 +0000

    id from the header

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from urllib import request
+    from sys import argv
+
+    req = request.Request(argv[1])
+    with request.urlopen(req) as response:
+        html = response.info()
+        html = dict(html)
+        print(html["X-Request-Id"])
