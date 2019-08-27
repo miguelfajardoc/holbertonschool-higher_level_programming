973e34d1bbbe73cb0cb9a9a2f69e04af852e2d6c
miguelfajardoc <775@holbertonschool.com>
2019-08-27 01:26:49 +0000

    json api

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from requests import get, post
+    from sys import argv
+
+    try:
+        data = {'q': argv[1]}
+    except:
+        data = {'q': ''}
+
+    try:
+        req = post("http://0.0.0.0:5000/search_user", data)
+        if req.json() == "":
+            print("No result")
+        else:
+            print("[{}] {}".format(req.json()['id'], req.json()['name']))
+    except:
+        print("No result")
