343d2c97b6d974fe2e06400b2702799b3c6ddba0
miguelfajardoc <775@holbertonschool.com>
2019-08-27 02:32:46 +0000

    mygithub

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from requests import get, post
+    from sys import argv
+
+
+    req = get("https://api.github.com/user", auth=(argv[1], argv[2]))
+    print(req.json()["id"])
