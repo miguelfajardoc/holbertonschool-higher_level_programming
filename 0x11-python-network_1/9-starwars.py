530178fdd39b3fde9d6ac40e40125d8bf75f1983
miguelfajardoc <775@holbertonschool.com>
2019-08-27 02:17:51 +0000

    star wars

---
+++ 0x11-python-network_1/9-starwars.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from requests import get, post
+    from sys import argv
+
+    datas = {'search': argv[1]}
+
+    req = post("https://swapi.co/api/people",params=datas)
+    print("Number of results: {}".format(req.json().get("count")))
+    for item in req.json()["results"]:
+        print(item["name"])
