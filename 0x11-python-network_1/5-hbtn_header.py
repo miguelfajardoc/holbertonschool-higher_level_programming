2b908ab0b970664bace91b342e9a0a0dbb890822
miguelfajardoc <775@holbertonschool.com>
2019-08-27 00:10:16 +0000

    add try except

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -4,5 +4,8 @@ if __name__ == "__main__":
     from requests import get
     from sys import argv
 
-    req = get(argv[1])
-    print(req.headers["X-Request-Id"])
+    try:
+        req = get(argv[1])
+        print(req.headers["X-Request-Id"])
+    except:
+        pass
