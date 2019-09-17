86bcd3e211c6b6224db86ed0e49459d211fe5c7c
miguelfajardoc <775@holbertonschool.com>
2019-09-17 22:30:57 +0000

    multiC

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,7 +4,7 @@ let x = Number(process.argv[2]);
 if (x) {
   while (x !== 0) {
     console.log('C is fun');
-    x--;
+    if (x < 0) { x++; } else { x--; }
   }
 } else {
   console.log('Missing number of occurrences');
