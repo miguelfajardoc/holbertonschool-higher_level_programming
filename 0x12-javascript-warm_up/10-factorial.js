e4eef3ae1dfd6251bbae993889e8c495a0ae7bbc
miguelfajardoc <775@holbertonschool.com>
2019-09-17 23:32:18 +0000

    factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -4,11 +4,10 @@ const x = parseInt(process.argv[2]);
 
 function factorial (a) {
   if (!a) { return a; }
-  if (a === 1 || a === - 1) { return 1; }
+  if (a === 1 || a === -1) { return 1; }
   if (a > 1) {
     return (a * factorial(--a));
-  }
-  else {
+  } else {
     return (a * factorial(++a));
   }
 }
