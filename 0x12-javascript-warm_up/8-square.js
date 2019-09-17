02b52b2f411a099de87b92b5c78fde04f8ea7b89
miguelfajardoc <775@holbertonschool.com>
2019-09-17 22:56:56 +0000

    square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,12 +5,12 @@ let y = x;
 let string = '';
 if (x) {
   while (x !== 0) {
-    if (x < 0){ break; }
-    while (y !== 0){
+    if (x < 0) { break; }
+    while (y !== 0) {
       string += 'X';
       y--;
-      }
-      console.log(string);
+    }
+    console.log(string);
     x--;
   }
 } else {
