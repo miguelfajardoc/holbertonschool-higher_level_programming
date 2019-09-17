d361e44c3a4c35524624c6308cd76678c27f34fe
miguelfajardoc <775@holbertonschool.com>
2019-09-17 23:25:15 +0000

    fatorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const x = Number(process.argv[2]);
+const x = parseInt(process.argv[2]);
 
 function factorial (a) {
   if (!a) { return a; }
-  if (a === 1) { return 1; }
+  if (a <= 1) { return 1; }
   return (a * factorial(--a));
 }
 
