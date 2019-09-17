ed158992ba66d09ec7289589fa22d4eedf2f4609
miguelfajardoc <775@holbertonschool.com>
2019-09-17 21:46:59 +0000

    fix semistandard

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 
-let sentence = process.argv[2] + " is " + process.argv[3]
-console.log(sentence)
+const sentence = process.argv[2] + ' is ' + process.argv[3];
+console.log(sentence);
