b4df98e95cff36c0e306f6d9db7c235bb6594904
miguelfajardoc <775@holbertonschool.com>
2019-09-24 02:23:24 +0000

    print rectangle

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (Number.isInteger(w) & Number.isInteger(h)) {
+      if (w > 0 & h > 0) {
+        this.width = w;
+        this.height = h;
+      }
+    }
+  }
+  print() {
+    let string = '';
+    let i = 0;
+    for (i = 0; i < this.width; i++) {
+      string +='X';
+    }
+    for (i = 0; i < this.height; i++) {
+      console.log(string);
+    }
+  } 
+}
+
+module.exports = Rectangle;
