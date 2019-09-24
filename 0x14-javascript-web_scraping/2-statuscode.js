b0424fed0958ea188d9ccea32e3542b5ec82bbdf
miguelfajardoc <775@holbertonschool.com>
2019-09-24 21:21:30 +0000

    status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const fs = require('request');
+const path = process.argv[2];
+
+fs(path, function (error, response) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(response.statusCode);
+  }
+});
