a0666a09c827727935c18bf4a15d255cc0a7bcb0
miguelfajardoc <775@holbertonschool.com>
2019-09-25 00:12:52 +0000

    fix semistandard

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,19 +2,18 @@
 
 const req = require('request');
 const path = process.argv[2];
-let todos = {};
-let va = 0;
+const todos = {};
 
 req(path, function (error, respond, body) {
   const tareas = JSON.parse(body);
   if (error) { console.log(error); }
   for (const i in tareas) {
     if (tareas[i].completed === true) {
-      let n = tareas[i].userId
+      const n = tareas[i].userId;
       if (n in todos) {
-	todos[tareas[i].userId]++;
+        todos[tareas[i].userId]++;
       } else {
-      todos[tareas[i].userId] = 1;
+        todos[tareas[i].userId] = 1;
       }
     }
   }
